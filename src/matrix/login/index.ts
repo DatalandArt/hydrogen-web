379ce1e77ca1d13e32ad79a95afb4376f9076133
Alexander Morosow <alex@refikanadolstudio.com>
2024-12-12 11:51:53 -0800

    make type import explicit

--- src/matrix/login/index.ts
+++ src/matrix/login/index.ts
@@ -2,6 +2,6 @@ import {PasswordLoginMethod} from "./PasswordLoginMethod";
 import {SSOLoginHelper} from "./SSOLoginHelper";
 import {TokenLoginMethod} from "./TokenLoginMethod";
 import {JWTLoginMethod} from "./JWTLoginMethod";
-import {ILoginMethod} from "./LoginMethod";
+import type {ILoginMethod} from "./LoginMethod";
 
 export {PasswordLoginMethod, SSOLoginHelper, TokenLoginMethod, JWTLoginMethod, ILoginMethod};
